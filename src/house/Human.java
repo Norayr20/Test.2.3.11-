fdad52c360710cc0630009686afb7153e4778c75
Norayr <asoyannorayr99@gmail.com>
2024-02-25 17:43:56 +0300

    add

---
+++ src/house/Human.java
@@ -0,0 +1,24 @@
+package house;
+
+public class Human {
+    private String secret;
+    protected String news;
+    String experience;
+    public String gossip;
+
+    private String getSecret() {
+        return secret;
+    }
+
+    protected String getNews() {
+        return news;
+    }
+
+    String getExperience() {
+        return experience;
+    }
+
+    public String getGossip() {
+        return gossip;
+    }
+}
---
+++ src/house/Main.java
@@ -0,0 +1,15 @@
+package house;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println(human.news);
+        System.out.println(human.experience);
+        System.out.println(human.gossip);
+        System.out.println(human.getNews());
+        System.out.println(human.getExperience());
+        System.out.println(human.getGossip());
+    }
+}
+
+
